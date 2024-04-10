25046d9febcd0ba400faa90c5c7888523ad867e9
Aayush <aayushsoni424@gmail.com>
2024-04-10 21:49:22 +0530

    Create Flask app

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    from app.models import store_status, business_hours, timezone_info
+
+    with app.app_context():
+        db.create_all()
+
+    return app
